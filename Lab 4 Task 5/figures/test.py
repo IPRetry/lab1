5403370d6a5be3cbbb6d679ca4f45e32e83c6108
IPRetry <113111993+IPRetry@users.noreply.github.com>
2022-12-25 12:58:40 +0300

    Update test.py

--- Lab 4 Task 5/figures/test.py
+++ Lab 4 Task 5/figures/test.py
@@ -1,1 +1,13 @@
+from figures import all
 
+r = 9
+a = 8 
+b = 5 
+c = 7
+
+circle_perimeter(r)
+circle_perimeter(r)
+square_area(a)
+square_perimeter(a)
+triangle_area(a, b, c)
+triangle_perimeter(a, b, c)
